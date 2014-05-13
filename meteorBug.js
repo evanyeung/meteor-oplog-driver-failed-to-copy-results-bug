8f4665ab1842fd14f0ee2b4850ff43a2843bce25
Evan Yeung <evan.yeung1@gmail.com>
2014-05-13 15:32:47 -0400

    initial commit

---
+++ meteorBug.js
@@ -0,0 +1,16 @@
+if (Meteor.isClient) {
+  Deps.autorun(function(){
+    Meteor.subscribe('userData');
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.publish('userData', function(){
+    return Meteor.users.find({_id: this.userId}, {fields: {extraField: 1}});
+  });
+
+  Accounts.onCreateUser(function(options, user){
+    user.extraField = 'a';
+    return user;
+  });
+}
